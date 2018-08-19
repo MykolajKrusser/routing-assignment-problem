512bbae247a118e357785a0a91fd35186337b14a
Nicolas <nicolaskrusser@gmail.com>
2018-08-19 13:19:41 +0200

    added redirect and 404 error page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, NavLink} from 'react-router-dom';
+import {BrowserRouter, Route, NavLink, Switch, Redirect} from 'react-router-dom';
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
+import noMatch from './component/404/404';
 
 class App extends Component {
   render () {
     return (
       <BrowserRouter>
         <div className="App">
-          <ol style={{textAlign: 'left'}}>
-            <li>Add a 404 error page and render it for any unknown routes</li>
-            <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
-          </ol>
           <nav>
-          <ul>
-            <li><NavLink to='/users'>Users</NavLink></li>
-            <li><NavLink to='/courses'>Courses</NavLink></li>
+          <ul style={{listStyle:"none"}}>
+            <li  style={{display:"inline-block", margin:"0 20px 0 0"}}><NavLink to='/users'>Users</NavLink></li>
+            <li  style={{display:"inline-block"}}><NavLink to='/courses'>Courses</NavLink></li>
           </ul>
           </nav>
-          <Route path='/users' component={Users}/>
-          <Route path='/courses' component={Courses}/>
+          <Switch>
+            <Route path='/users' component={Users}/>
+            <Route path='/courses' component={Courses}/>
+            <Redirect from='/all-courses' to='/courses'/>
+            <Route component={noMatch}/>
+          </Switch>
         </div>
       </BrowserRouter>
     );
